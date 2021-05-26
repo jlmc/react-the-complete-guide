b70bef6fcb060db7ea62940c0d75c0f0c779e9b3
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-26 23:22:29 +0100

    props Parameters

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,36 @@
 import ExpensiveItem from "./components/ExpenseItem"
 
 function App() {
+    const expensives = [
+        {
+            id: "1",
+            title: "Car Insurance 1",
+            price: 294.67,
+            date: new Date(2021, 5, 28)
+        },
+        {
+            id: "2",
+            title: "Car Insurance 2",
+            price: 294.67,
+            date: new Date(2020, 12, 28)
+        },
+        {
+            id: "3",
+            title: "Car Insurance 3",
+            price: 56.67,
+            date: new Date(2020, 1, 28)
+        }
+    ];
+
     return (
-      <div>
-        <h2>Let's get started!</h2>
-        <ExpensiveItem></ExpensiveItem>
-      </div>
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpensiveItem
+                title={expensives[0].title}
+                date={expensives[0].date}
+                price={expensives[0].price}></ExpensiveItem>
+        </div>
     );
-  }
-  
-  export default App;   
-  
+}
+
+export default App;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css'
 
-function ExpensiveItem() {
-  const expenseDate = new Date(2021, 5, 28);
-  const expenseTitle = "Car Insurance";
-  const expensePrice = 294.67;
+function ExpensiveItem(props) {
 
+
+  //const expenseDate = new Date(2021, 5, 28);
+  //const expenseTitle = "Car Insurance";
+  //const expensePrice = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
