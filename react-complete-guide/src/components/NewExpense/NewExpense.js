a9d6f5ed0c098e0d14d1d017e929689fce86cd44
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-27 17:01:20 +0100

    Editable form

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(props) {
+  const [isEditable, setIsEditable] = useState(false);
+
   function saveExpenseData(enteredExpenseData) {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
 
-    console.log(`### ${JSON.stringify(expenseData)}`);
-
     props.onExpenseAdded(expenseData);
+  }
+
+  function stopEditingHandler() {
+    setIsEditable(false);
+  }
+
+  function startEditingHandler() {
+    setIsEditable(true);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEditable && <button onClick={startEditingHandler}>Add New Expense</button>}
+
+      {isEditable && <ExpenseForm onSaveExpenseData={saveExpenseData} onCancel={stopEditingHandler} />}
     </div>
   );
 }
