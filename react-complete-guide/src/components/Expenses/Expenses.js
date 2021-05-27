6d7cf30f7d0d6801d888c2786d45046d58d76720
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-27 15:18:58 +0100

    Applying filter to the Stateful list

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -7,21 +7,33 @@ import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
   //let items = props.items;
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('all');
 
-  function onChangeFilterHandler(filterData) {
-    console.log(filterData);
+  function onChangeFilterHandler(filteredYear) {
+    console.log(filteredYear);
     setFilteredYear(filteredYear);
   }
 
+  const filterExpenses = props.items.filter(expense => {
+    let valid = 'all' === filteredYear || filteredYear === expense.date.getFullYear().toString();
+    return valid;
+  });
+
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={onChangeFilterHandler} />
-      {props.items.map(expense => {
+      <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeFilterHandler} />
+      {filterExpenses.map(expense => {
         return <ExpensiveItem key={expense.id} title={expense.title} price={expense.price} date={expense.date} />;
       })}
     </Card>
   );
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 }
 
 export default Expenses;
