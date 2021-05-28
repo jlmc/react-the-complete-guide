b223a44f91cf7b19363565bcd2050dd695c7af3c
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-28 16:02:00 +0100

    80 - On Add User Component

--- hands-up0-app/src/App.js
+++ hands-up0-app/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
 
+const DUMMY_USERS = [];
+
 function App() {
-  return <AddUser />;
+  function addUser(newUser) {
+    let user = {
+      ...newUser,
+      id: Math.random().toString(),
+    };
+
+    DUMMY_USERS.push(user);
+
+    console.log(DUMMY_USERS);
+  }
+
+  return <AddUser addUser={addUser} />;
 }
 
 export default App;
--- hands-up0-app/src/components/Users/AddUser.js
+++ hands-up0-app/src/components/Users/AddUser.js
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddUser = props => {
+  let [isValid, setIsValid] = useState(true);
+  let [_username, setUsername] = useState('');
+  let [_age, setAge] = useState('1');
+
+  function onUsernameChangeHandler(event) {
+    let value = event.target.value;
+    setUsername(value);
+  }
+
+  function onAgeChangeHandler(event) {
+    let value = event.target.value;
+    setAge(value);
+  }
+
   function addUserHandler(event) {
     event.preventDefault();
+
+    if (_username.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
+    if (!isNumeric(_age) || +_age <= 0) {
+      setIsValid(false);
+      return;
+    }
+
+    let newUser = {
+      userName: _username,
+      age: +_age,
+    };
+
+    console.log(newUser);
+
+    props.addUser(newUser);
+
+    setUsername('');
+    setAge('1');
+  }
+
+  function isNumeric(value) {
+    return /^\d+$/.test(value);
   }
 
   return (
-    <Card className={styles.input}>
+    <Card className={`${styles.input} ${!isValid && styles.invalid}`}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={_username} onChange={onUsernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" min="0" step="1" type="number" value={_age} onChange={onAgeChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
@@ -23,3 +63,14 @@ const AddUser = props => {
 };
 
 export default AddUser;
+
+/*
+  const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
+    setEnteredValue(event.target.value);
+  };
+
+ */
