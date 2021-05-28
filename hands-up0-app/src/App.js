2b5031fd760a69103f520c40fd96ef21a23ee676
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-28 16:44:23 +0100

    80-Managed Users List

--- hands-up0-app/src/App.js
+++ hands-up0-app/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 const DUMMY_USERS = [];
 
 function App() {
-  function addUser(newUser) {
+  const [_users, setUsers] = useState(DUMMY_USERS);
+
+  function addUserHandler(newUser) {
     let user = {
       ...newUser,
       id: Math.random().toString(),
     };
 
-    DUMMY_USERS.push(user);
-
-    console.log(DUMMY_USERS);
+    setUsers(prevUsersList => {
+      return [...prevUsersList, user];
+    });
   }
 
   return (
     <div>
-      <AddUser addUser={addUser} />
-      <UserList users={DUMMY_USERS} />
+      <AddUser addUser={addUserHandler} />
+      <UserList users={_users} />
     </div>
   );
 }
