5c359667468006a0549aa6094b1039393f54f420
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-29 14:42:42 +0100

    10 - Simple Useeffect that run once when the app start

--- 10-sideeffect/examples/src/App.js
+++ 10-sideeffect/examples/src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+const IS_LOGGED_IN = "isLoggedIn";
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(
+    () => {
+      let storedUserLoggedInInformation = localStorage.getItem(IS_LOGGED_IN);
+      if (storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true)
+      }
+  }, [])
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem(IS_LOGGED_IN, "1")
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem(IS_LOGGED_IN)
     setIsLoggedIn(false);
   };
 
