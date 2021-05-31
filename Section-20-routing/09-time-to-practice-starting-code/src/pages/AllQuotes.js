ebf21f02ba7687efc6b3043d50dbee6256925a61
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 00:57:48 +0100

    wip1

--- Section-20-routing/09-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,8 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id: 1, author: 'John', text: 'Always learning!'},
+    {id: 2, author: 'Ali', text: 'Greater off all times !'},
+    {id: 3, author: 'Duke', text: 'Java Rocks!'},
+]
+
 const AllQuotes = () => {
-
-
-
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES}/>
 };
 
 export default AllQuotes;
