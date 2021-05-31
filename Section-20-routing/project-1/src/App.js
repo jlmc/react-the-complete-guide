d20fcfe5af00c2793d5d8706a80415e220709eee
Joao Morgado <joao.morgado@talkdesk.com>
2021-05-31 18:05:23 +0100

    Section 20: Basic router

--- Section-20-routing/project-1/src/App.js
+++ Section-20-routing/project-1/src/App.js
@@ -1,9 +1,26 @@
+import {Route} from 'react-router-dom';
+import Products from "./components/pages/Products";
+import Welcome from "./components/pages/Welcome";
+
 function App() {
-  return (
+
+    return (
+      <div>
+          <Route path="/welcome">
+              <Welcome/>
+          </Route>
+          <Route path="/products">
+            <Products/>
+          </Route>
+      </div>
+    );
+
+
+/*  return (
     <div>
       <h2>Let's get started!</h2>
     </div>
-  );
+  );*/
 }
 
 export default App;
