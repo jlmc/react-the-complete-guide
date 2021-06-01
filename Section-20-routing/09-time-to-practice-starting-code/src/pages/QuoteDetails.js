fe42a9f3ad81fcaf06bdfcfdf331676eda042f9f
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 14:25:46 +0100

    Section 20: 286. Writing More Flexible Routing Code

--- Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,5 +1,5 @@
 import {Fragment} from 'react';
-import {Route, useParams} from 'react-router-dom';
+import {Link, Route, useParams} from 'react-router-dom';
 import {DUMMY_QUOTES} from "./dummy-quotes";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -17,11 +17,20 @@ const QuoteDetail = () => {
 
     return (
         <Fragment>
-            <HighlightedQuote  text={quote.text}  author={quote.author}/>
-            <Route path={`/quotes/${quote.quoteId}/comments`}>
-                <Comments/>
+            <HighlightedQuote text={quote.text} author={quote.author}/>
+
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Commments</Link>
+                </div>
             </Route>
 
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments/>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}`}>close comments</Link>
+                </div>
+            </Route>
 
         </Fragment>
     );
