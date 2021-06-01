42ffce7501b78415645297ecd8c6cc359f1d63e1
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 14:31:14 +0100

    Section 20: 286. Writing More Flexible Routing Code

--- Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,12 +1,14 @@
 import {Fragment} from 'react';
-import {Link, Route, useParams} from 'react-router-dom';
+import {Link, Route, useParams, useRouteMatch} from 'react-router-dom';
 import {DUMMY_QUOTES} from "./dummy-quotes";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
     const params = useParams();
+    const match = useRouteMatch();
     console.log(params)
+
 
     const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
 
@@ -19,13 +21,13 @@ const QuoteDetail = () => {
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author}/>
 
-            <Route path={`/quotes/${params.quoteId}`} exact>
+            <Route path={match.path} exact>
                 <div className='centered'>
                     <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Commments</Link>
                 </div>
             </Route>
 
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Route path={`${match.path}/comments`}>
                 <Comments/>
                 <div className='centered'>
                     <Link className='btn--flat' to={`/quotes/${params.quoteId}`}>close comments</Link>
