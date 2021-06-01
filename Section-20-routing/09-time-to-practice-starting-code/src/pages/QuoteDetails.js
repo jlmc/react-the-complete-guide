5be5c8ac38ef1847c5b8be92c8e85079321dc4c8
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 18:05:24 +0100

    Section 20: 287. Sending & Getting Quote Data via Http Single Quote Details

--- Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,18 +1,45 @@
-import {Fragment} from 'react';
+import {Fragment, useEffect} from 'react';
 import {Link, Route, useParams, useRouteMatch} from 'react-router-dom';
-import {DUMMY_QUOTES} from "./dummy-quotes";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import useHttp from "../components/hooks/use-http";
+import {getSingleQuote} from "../components/lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
     const params = useParams();
     const match = useRouteMatch();
+    const {quoteId} = params
     console.log(params)
+    console.log(quoteId)
 
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
 
-    if (!quote) {
+    const {sendRequest, status, data: quote, error} = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        console.log('---> 1... Send HTTP Request - ' + quoteId)
+        sendRequest(quoteId)
+    }, [sendRequest, quoteId])
+
+    //const quote = DUMMY_QUOTES.find(quote => quote.id === quoteId);
+
+
+    if (status === 'pending') {
+        console.log('---> 3... pending')
+        return <div className='centered'>
+            <LoadingSpinner/>
+        </div>
+    }
+
+    if (error) {
+        console.log('---> 4... error')
+        return <div className='centered focused'>
+            <p>{error}</p>
+        </div>
+    }
+
+    if (!quote.text) {
         // security
         return <p>No Quote found!!!!</p>
     }
