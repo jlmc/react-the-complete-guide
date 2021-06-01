95095e81d12220ba4841622a051c16d67ac803b2
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 17:54:34 +0100

    Section 20: 287. Sending & Getting Quote Data via Http

--- Section-20-routing/09-time-to-practice-starting-code/src/pages/NewQuote.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,11 +1,24 @@
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from 'react-router-dom'
+import useHttp from "../components/hooks/use-http";
+import {addQuote} from "../components/lib/api";
+import {useEffect} from "react";
 
 const NewQuote = () => {
     const history = useHistory();
 
+    const {sendRequest, status} = useHttp(addQuote);
+
+    useEffect(() => {
+        if (status === 'completed') {
+            history.push('/quotes')
+        }
+    }, [status, history])
+
     const onAddQuoteHandler = (quote) => {
         console.log(quote);
+
+        sendRequest(quote)
 
         /**
          * push vs replace
@@ -14,11 +27,11 @@ const NewQuote = () => {
          * - replace is like a redirect
          * - push add a new page
          */
-        history.push('/quotes')
+        //history.push('/quotes')
 
     }
 
-    return <QuoteForm onAddQuote={onAddQuoteHandler}/>
+    return <QuoteForm isLoading={ status === 'pending'} onAddQuote={onAddQuoteHandler}/>
 };
 
 export default NewQuote;
