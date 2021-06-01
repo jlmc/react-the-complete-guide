f205119adefeb0c5ed576b28af3bcc1496e2d167
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-01 18:52:52 +0100

    Section 20: 287. Sending & Getting Quote Data via Http Comments Sections with some bugs

--- Section-20-routing/09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
+++ Section-20-routing/09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
@@ -1,29 +1,48 @@
-import { useRef } from 'react';
+import {useEffect, useRef} from 'react';
 
 import classes from './NewCommentForm.module.css';
+import useHttp from "../hooks/use-http";
+import {addComment} from "../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const NewCommentForm = (props) => {
-  const commentTextRef = useRef();
+    const commentTextRef = useRef();
+    const {quoteId, onAddedComment} = props;
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
+    const {sendRequest, status, error} = useHttp(addComment, true);
 
-    // optional: Could validate here
+    const submitFormHandler = (event) => {
+        event.preventDefault();
 
-    // send comment to server
-  };
+        // optional: Could validate here
+        let text = commentTextRef.current.value;
 
-  return (
-    <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
-      </div>
-    </form>
-  );
+        // send comment to server
+        sendRequest({quoteId: quoteId, commentData: {text: text}})
+    };
+
+    useEffect(() => {
+        if (status === 'completed' && !error) {
+            onAddedComment();
+        }
+    }, [status, error, onAddedComment])
+
+    return (
+        <form className={classes.form} onSubmit={submitFormHandler}>
+            {status === 'pending' &&
+                <div className='centered'>
+                    <LoadingSpinner/>
+                </div>
+            }
+            <div className={classes.control} onSubmit={submitFormHandler}>
+                <label htmlFor='comment'>Your Comment</label>
+                <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+            </div>
+            <div className={classes.actions}>
+                <button className='btn'>Add Comment</button>
+            </div>
+        </form>
+    );
 };
 
 export default NewCommentForm;
