756e9aaca1aafb967141158dafdbf60bea92332d
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-03 18:16:37 +0100

    Section 26: 385. Grouping Tests Together With Test Suites

--- Section-26-Tests/example-project/src/components/Greeting.test.js
+++ Section-26-Tests/example-project/src/components/Greeting.test.js
@@ -1,21 +1,23 @@
 import {render, screen} from '@testing-library/react';
 import Greeting from "./Greeting";
 
-test('renders hello world as a texr+t', () => {
-    // AAA rule
-    // arrange
-    // act
-    // assert
+describe("Greeting component", () => {
+    test('renders hello world as a texr+t', () => {
+        // AAA rule
+        // arrange
+        // act
+        // assert
 
-    // arrange
-    render(<Greeting/>);
+        // arrange
+        render(<Greeting/>);
 
-    // act: do nothing
+        // act: do nothing
 
-    // assert
-    const h2 = screen.getByText("Hello world!", {
-        exact: true
+        // assert
+        const h2 = screen.getByText("Hello world!", {
+            exact: true
+        });
+
+        expect(h2).toBeInTheDocument();
     });
-
-    expect(h2).toBeInTheDocument();
 });
