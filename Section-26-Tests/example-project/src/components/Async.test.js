032c204495fab9c6fede7bd55e6af75be63f0654
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-03 19:02:11 +0100

    Section 26: 389. Working With Mocks

--- Section-26-Tests/example-project/src/components/Async.test.js
+++ Section-26-Tests/example-project/src/components/Async.test.js
@@ -13,4 +13,19 @@ describe('Async component', () => {
         expect(listItemElements).not.toHaveLength(0);
 
     });
+
+    test('renders post id request succeeds with MOCK', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{ id: 'p1', title: 'First post' }],
+        });
+
+        render(<Async/>);
+
+        // getByRole will fails if more than one elements is find
+        //const listItemElements = screen.getByRole('listitem');
+        //const listItemElements = screen.getAllByRole('listitem');
+        const listItemElements = await screen.findAllByRole('listitem', {}, {}); // find methods returns a promise,
+        expect(listItemElements).not.toHaveLength(0);
+    });
 });
