4d23dd58e9881e937259b0365d0c4ed711b79ccf
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-05 20:49:01 +0100

    Section-11: Cart Modal

--- Section-11-practice-food-order-app/food-order/src/App.js
+++ Section-11-practice-food-order-app/food-order/src/App.js
@@ -1,11 +1,13 @@
 import {Fragment} from "react";
 import Header from "./componens/Layout/Header";
 import Meals from "./componens/Meals/Meals";
+import Cart from "./componens/Card/Cart";
 
 function App() {
   return (
 
       <Fragment>
+          <Cart></Cart>
           <Header/>
           <main>
               <Meals/>
