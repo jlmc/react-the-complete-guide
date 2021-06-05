85290f981d80a0b67597e96300c27582d616cd92
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-05 17:26:54 +0100

    Section-11: main section just styling

--- Section-11-practice-food-order-app/food-order/src/App.js
+++ Section-11-practice-food-order-app/food-order/src/App.js
@@ -1,11 +1,15 @@
 import {Fragment} from "react";
 import Header from "./componens/Layout/Header";
+import Meals from "./componens/Meals/Meals";
 
 function App() {
   return (
 
       <Fragment>
           <Header/>
+          <main>
+              <Meals/>
+          </main>
       </Fragment>
   );
 }
