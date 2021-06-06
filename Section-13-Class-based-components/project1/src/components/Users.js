bcc4804f4aefd3009207911dce30e286ab4807dd
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-06 22:30:14 +0100

    Section 13 - Class based component Using state

--- Section-13-Class-based-components/project1/src/components/Users.js
+++ Section-13-Class-based-components/project1/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {Component} from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,6 +9,50 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+
+class Users extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    // define/initialize the states
+    this.state = {
+      showUsers: true,
+      dummy: 'Dukes'
+    };
+  }
+
+  toggleUsersHandler() {
+    // setShowUsers((curState) => !curState);
+
+    // this will merge the changes with the existing state, we don't have to pass all the values like we do in functional components
+    this.setState((currentState) => {
+      return {
+        showUsers: !currentState.showUsers
+      }
+    });
+  }
+
+  render() {
+    const usersList = (
+        <ul>
+          {DUMMY_USERS.map((user) => (
+              <User key={user.id} name={user.name} />
+          ))}
+        </ul>
+    );
+
+    return <div className={classes.users}>
+      <button onClick={this.toggleUsersHandler.bind(this)}>
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
+      </button>
+      {this.state.showUsers && usersList}
+    </div>;
+  }
+}
+
+
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -33,5 +77,7 @@ const Users = () => {
     </div>
   );
 };
+
+ */
 
 export default Users;
