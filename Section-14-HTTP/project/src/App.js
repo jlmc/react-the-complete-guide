4202a4fa9e5e5c1661fd32e53d689929c44d06fa
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-07 13:12:41 +0100

    Section 14 - Handling loading data

--- Section-14-HTTP/project/src/App.js
+++ Section-14-HTTP/project/src/App.js
@@ -4,29 +4,22 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-    const dummyMovies = [
-        {
-            id: 1,
-            title: 'Some Dummy Movie',
-            openingText: 'This is the opening text of the movie',
-            releaseDate: '2021-05-18',
-        },
-        {
-            id: 2,
-            title: 'Some Dummy Movie 2',
-            openingText: 'This is the second opening text of the movie',
-            releaseDate: '2021-05-19',
-        },
-    ];
 
 
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     /* useEffect(() => {
          fetchMoviesHandler();
      }, []);*/
 
     function fetchMoviesHandler() {
+        console.log(isLoading)
+
+        setIsLoading(true);
+
+
+
         fetch("https://swapi.dev/api/films/", {method: 'GET'})
             .then((response) => {
                 return response.json();
@@ -44,11 +37,22 @@ function App() {
                     });
 
                 setMovies(transformedMovies);
+
+                setIsLoading(false);
+
             });
     }
 
 
     async function asyncFetchMoviesHandler() {
+        setMovies([])
+        setIsLoading((prevState => {
+
+            setIsLoading(true);
+
+        }));
+
+
         const response = await fetch("https://swapi.dev/api/films/", {method: 'GET'});
         const data = await response.json();
 
@@ -65,16 +69,24 @@ function App() {
             });
 
         setMovies(transformedMovies);
+        setIsLoading((prevState => {
+
+            setIsLoading(false);
+
+        }));
     }
 
 
     return (
         <React.Fragment>
             <section>
-                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+                <button onClick={asyncFetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
                 <MoviesList movies={movies}/>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && <p>Found no movies!</p>}}
+                {isLoading && <p>loading ...</p>}
             </section>
         </React.Fragment>
     );
