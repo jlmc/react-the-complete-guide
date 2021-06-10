bb83835a7af048155b87b108f20af58609b316f9
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-10 20:03:38 +0100

    Section 16 - Clean up

--- Section-16-Forms/01-starting-project/src/components/SimpleInput.js
+++ Section-16-Forms/01-starting-project/src/components/SimpleInput.js
@@ -1,27 +1,27 @@
-import {useEffect, useRef, useState} from "react";
+import {useState} from "react";
 
 const SimpleInput = (props) => {
     const [name, setName] = useState('');
-    const nameInputRef = useRef();
-
-    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+    const enteredNameIsValid = name.trim().trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+    /*
     useEffect(() => {
+        // this can be some HTTP validation
         if (enteredNameIsValid) {
             console.log('Name Input is valid!');
         }
     }, [enteredNameIsValid]);
-
+     */
 
     function onNameInputChangeHandler(event) {
         setName(event.target.value)
-
         // using the input value and not the state value because the state could not updated yet
-        if (event.target.value.trim() !== '') {
-            setEnteredNameIsValid(true);
-        }
+        //if (event.target.value.trim() !== '') {
+        //    setEnteredNameIsValid(true);
+        //}
     }
 
     function onFormSubmitHandler(event) {
@@ -29,32 +29,18 @@ const SimpleInput = (props) => {
 
         setEnteredNameTouched(true);
 
-        // from state
-        console.log(`===> ${name}`)
-
-        // from reference
-        const value = nameInputRef.current.value;
-        console.log(`###> ${value}`);
-
-        if (name.trim() === '') {
-            setEnteredNameIsValid(false)
+        if (!enteredNameIsValid) {
             return;
         }
 
-        // if every thing is fine then we can clean the form
-        setEnteredNameIsValid(true)
+        setEnteredNameTouched(false)
         setName('');
     }
 
     function onNameInputBlurHandler(event) {
         setEnteredNameTouched(true);
-
-        if (name.trim() === '') {
-            setEnteredNameIsValid(false);
-        }
     }
 
-    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
     const nameInputClasses = nameInputIsInvalid
         ? 'form-control invalid'
         : 'form-control';
@@ -66,7 +52,6 @@ const SimpleInput = (props) => {
                 <input type='text' id='name'
                        onChange={onNameInputChangeHandler}
                        onBlur={onNameInputBlurHandler}
-                       ref={nameInputRef}
                        value={name}
                 />
                 {nameInputIsInvalid && (
