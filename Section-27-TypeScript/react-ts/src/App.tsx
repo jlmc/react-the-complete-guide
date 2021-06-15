45c09d9c1e801899e67c05454f71ec57387fd4f8
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-15 01:46:36 +0100

    Section-27 - 406. Adding a Data Model

--- Section-27-TypeScript/react-ts/src/App.tsx
+++ Section-27-TypeScript/react-ts/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Todos from "./components/Todos";
+import Todo from "./models/Todo";
+
+
 
 function App() {
+
+    const todos = [
+
+        new Todo("Learn React"),
+        new Todo("Learn Type Script"),
+    ];
+
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn TypeScript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
--- Section-27-TypeScript/react-ts/src/components/Todos.tsx
+++ Section-27-TypeScript/react-ts/src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Todo from "../models/Todo";
+import TodoItem from "./TodoItem";
 
 
 // React.FC - Functional Component
 //const Todos: React.FC = (props: React.PropsWithChildren<{}>) => {
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]}> = (props) => {
     return (
         <ul>
             {props.items.map(item => {
-                return <li key={item}>{item}</li>
+                return <TodoItem key={item.id} text={item.text}/>
             })}
          </ul>);
 }
