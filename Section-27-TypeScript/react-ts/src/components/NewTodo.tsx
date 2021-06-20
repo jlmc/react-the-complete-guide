2e7f3a8410c048917bd66fa5aebdc0764fbf235f
Joao Morgado <joao.morgado@talkdesk.com>
2021-06-20 16:00:38 +0100

    Section-27 - 412. Adding Styling

--- Section-27-TypeScript/react-ts/src/components/NewTodo.tsx
+++ Section-27-TypeScript/react-ts/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, {useRef} from "react";
+import classes from './NewTodo.module.css';
 
 const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
     //https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
@@ -21,7 +22,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
 
 
     return (
-        <form onSubmit={submitHandler}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <label htmlFor='text'>Todo text</label>
             <input type='text' id='text' ref={todoTextInputRef}/>
             <button>Add Todo</button>
--- Section-27-TypeScript/react-ts/src/components/Todos.tsx
+++ Section-27-TypeScript/react-ts/src/components/Todos.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Todo from "../models/Todo";
 import TodoItem from "./TodoItem";
+import styles from './Todos.module.css'
 
 
 // React.FC - Functional Component
 //const Todos: React.FC = (props: React.PropsWithChildren<{}>) => {
 const Todos: React.FC<{items: Todo[]}> = (props) => {
     return (
-        <ul>
+        <ul className={styles.todos}>
             {props.items.map(item => {
                 return <TodoItem key={item.id} text={item.text}/>
             })}
