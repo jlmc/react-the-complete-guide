7224deccb59b2d37b5beb5f3b5e8a1ec13f7fd62
Joao Costa <costajlmpp@gmail.com>
2021-11-01 22:11:04 +0000

    2.8. Shared components

--- Section-others/x-stocks/src/components/App/App.tsx
+++ Section-others/x-stocks/src/components/App/App.tsx
@@ -3,22 +3,24 @@ import React from 'react';
 // import ClassComponent from "../ClassComponent";
 import FunctionalComponent from "../FunctionComponent";
 import Header from "../Header";
-import Button from "../Button";
+import Button from "../shared/Button";
+import Container from "../shared/Container";
 
-function TestComponent () {
-    return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+function TestComponent() {
+    return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon"/>
 }
 
 function App() {
     return (
         <div>
             <Header title="x-stocks"/>
-
-            <Button content="OK"
-                    appendIcon={<TestComponent />}
-                    onClick={() => window.alert('Hello')}>
-                Ok
-            </Button>
+            <Container>
+                <Button content="OK"
+                        appendIcon={<TestComponent/>}
+                        onClick={() => window.alert('Hello')}>
+                    Ok
+                </Button>
+            </Container>
         </div>
     );
 }
