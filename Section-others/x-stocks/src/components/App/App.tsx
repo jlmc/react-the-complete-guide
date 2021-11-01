690994871419601c6fac7e4f3e4cc8fbe426f29a
Joao Costa <costajlmpp@gmail.com>
2021-11-01 21:20:20 +0000

    2.7. Props

--- Section-others/x-stocks/src/components/App/App.tsx
+++ Section-others/x-stocks/src/components/App/App.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 // import ClassComponent from "../ClassComponent";
 import FunctionalComponent from "../FunctionComponent";
 import Header from "../Header";
+import Button from "../Button";
+
+function TestComponent () {
+    return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
 
 function App() {
     return (
         <div>
             <Header title="x-stocks"/>
 
-            <FunctionalComponent name="Jack"/>
+            <Button content="OK"
+                    appendIcon={<TestComponent />}
+                    onClick={() => window.alert('Hello')}>
+                Ok
+            </Button>
         </div>
     );
 }
