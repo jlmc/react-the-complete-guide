80c49f929a32aef0b97c686d3923336b7e1f8e6e
Joao Costa <costajlmpp@gmail.com>
2021-11-02 01:55:08 +0000

    2.17. Product Form apply

--- Section-others/x-stocks/src/components/App/App.tsx
+++ Section-others/x-stocks/src/components/App/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import SimpleComponent from "../SimpleComponent";
 // import ClassComponent from "../ClassComponent";
 //import FunctionalComponent from "../FunctionComponent";
 import Header from "../Header";
 import Container from "../shared/Container";
 import Table, {TableHeader} from "../shared/Table";
-import Products from "../shared/Table/Table.mockdata";
-import ProductForm from "../Products/ProductForm";
+import Products, {Product} from "../shared/Table/Table.mockdata";
+import ProductForm, {ProductCreator} from "../Products/ProductForm";
 
 // function TestComponent() {
 //     return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon"/>
@@ -22,6 +22,16 @@ const headers: TableHeader[] = [
 function App() {
     //const [street, setStreet] = useState('')
 
+    const [products, setProducts] = useState(Products);
+
+    const handleProductFormSubmit = (productCreator: ProductCreator) => {
+        let product: Product = {
+            id: products.length + 1,
+            ...productCreator
+        }
+        setProducts([...products, product])
+    }
+
     return (
         <div>
             <Header title="x-stocks"/>
@@ -29,10 +39,10 @@ function App() {
 
                 <Table
                     headers={headers}
-                    data={Products}
+                    data={products}
                 />
 
-               <ProductForm/>
+               <ProductForm onSubmit={handleProductFormSubmit}/>
             </Container>
         </div>
     );
--- Section-others/x-stocks/src/components/Products/ProductForm.tsx
+++ Section-others/x-stocks/src/components/Products/ProductForm.tsx
@@ -2,23 +2,84 @@ import React, {useState} from "react";
 import Form from "../shared/Form";
 import Input from "../shared/Input";
 import Button from "../shared/Button";
+import {Product} from "../shared/Table/Table.mockdata";
 
-const initialFormState = {
-    name: '',
-    price: '',
-    stock: ''
+export interface ProductCreator {
+    name: string
+    price: number,
+    stock: number
 }
 
-const ProductForm: React.FC = (props) => {
+export interface ProductUpdater {
+    id: number,
+    name: string
+    price: number,
+    stock: number
+}
+
+export interface ProductFormProps {
+    product?: Product
+    onSubmit?: (product: ProductCreator) => void
+    onUpdate?: (product: ProductUpdater) => void
+}
+
+declare interface InitialFormState {
+    id?: number,
+    name: string,
+    price: string,
+    stock: string
+}
+
+const ProductForm: React.FC<ProductFormProps> = (props) => {
+
+    const getInitialFormState = (product?: Product) => {
+        return product ?
+            {
+                id: product.id,
+                name: product.name,
+                price: String(product.price),
+                stock: String(product.stock)
+            }
+            : {
+                name: '',
+                price: '',
+                stock: ''
+            };
+    }
+
+    const initialFormState: InitialFormState = getInitialFormState(props.product)
+
     const [form, setForm] = useState(initialFormState);
 
+    function onFormUpdate(event: React.FormEvent<HTMLFormElement>) {
+        if (!props.product) return
+
+        const productUpdater: ProductUpdater = {
+            id: props.product.id,
+            name: String(form.name),
+            price: parseFloat(form.price),
+            stock: Number(form.stock)
+        };
+
+        props.onUpdate && props.onUpdate(productUpdater)
+
+        setForm(initialFormState)
+    }
+
     function onFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-        console.log("----")
-        console.log(form)
+        const productCreator: ProductCreator = {
+            name: String(form.name),
+            price: parseFloat(form.price),
+            stock: Number(form.stock)
+        };
+
+        props.onSubmit && props.onSubmit(productCreator)
+
+        setForm(initialFormState)
     }
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value, name }: EventTarget & HTMLInputElement = event.target
+        const {value, name}: EventTarget & HTMLInputElement = event.target
 
         setForm({
             ...form,
@@ -27,10 +88,28 @@ const ProductForm: React.FC = (props) => {
     }
 
     return <React.Fragment>
-        <Form title="Product details" onSubmit={onFormSubmit}>
-            <Input label="Name" required placeholder="Cookie" name="name" onChange={handleInputChange}/>
-            <Input label="Price" required type="number" step="0.01" placeholder="99.12" name="price" onChange={handleInputChange}/>
-            <Input label="Stock" required type="number" min="0" step="0.01" placeholder="0" name="stock" onChange={handleInputChange}/>
+        <Form title="Product details" onSubmit={props.product ? onFormUpdate : onFormSubmit}>
+            <Input label="Name"
+                   value={form.name}
+                   required
+                   placeholder="Cookie"
+                   name="name"
+                   onChange={handleInputChange}/>
+            <Input label="Price"
+                   value={form.price}
+                   required
+                   type="number"
+                   step="0.01"
+                   placeholder="99.12"
+                   name="price"
+                   onChange={handleInputChange}/>
+            <Input label="Stock"
+                   value={form.stock}
+                   required type="number"
+                   min="0"
+                   step="0.01"
+                   placeholder="0"
+                   name="stock" onChange={handleInputChange}/>
             <Button>Save</Button>
         </Form>
     </React.Fragment>
