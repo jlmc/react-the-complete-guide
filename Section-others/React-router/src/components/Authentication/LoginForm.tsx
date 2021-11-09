d8c5a21cd0ea6286d85bb040dfcc788285ed7e9b
Joao Morgado <joao.morgado@talkdesk.com>
2021-11-09 19:00:43 +0000

    Redirect to home after signin

--- Section-others/React-router/src/components/Authentication/LoginForm.tsx
+++ Section-others/React-router/src/components/Authentication/LoginForm.tsx
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Input from "../shared/Input";
 import Button from "../shared/Button";
 import Form from "../shared/Form";
 import {useAppDispatch, useAppSelector} from "../../hooks";
 import {User} from "../../service/Authentication.service";
 import {Credentials, signIn} from "../../redux/Authentication/Authentication.actions";
+import {useNavigate} from "react-router-dom";
+import {PENDING, SUCCEEDED} from "../../redux/Authentication/Authentication.slice";
 
 const LoginForm = () => {
     const [form, setForm] = useState({
@@ -13,12 +15,15 @@ const LoginForm = () => {
     });
 
     const dispatch = useAppDispatch();
+    const navigate = useNavigate()
     const user: User | undefined = useAppSelector(state => state.authentication.user);
+    const loading: String | undefined = useAppSelector(state => state.authentication.loading);
 
-    /*
-      const dispatch = useDispatch();
-  const username = useSelector(state => state.authentication.username);
-     */
+    useEffect(() => {
+        if (user && loading === SUCCEEDED) {
+            navigate("/", {replace: true})
+        }
+    }, [navigate, user, loading])
 
     const handleLogin = () => {
         console.table(form)
@@ -30,25 +35,6 @@ const LoginForm = () => {
         dispatch(signIn(credentials))
     }
 
-
-/*    function loginHandle(event) {
-        event.preventDefault();
-        let passwordValue = passwordTextInput.current.value;
-        console.log("Password: " + passwordValue)
-        console.log("Username: " + usernameValue)
-
-        dispatch(authenticationSliceActions.login({
-            username: usernameValue,
-            password: passwordValue
-        }))
-    }*/
-
-    /*
-        useEffect(() => {
-        appDispatch(ProductActions.fetchAllProducts(1234))
-    }, [appDispatch])
-     */
-
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const {value, name} = event.target
 
@@ -59,25 +45,30 @@ const LoginForm = () => {
     }
 
     return (
-        <Form title="Login - x-stock" onSubmit={handleLogin}>
-            <Input
-                label="User"
-                name="user"
-                value={form.user}
-                onChange={handleInputChange}
-                placeholder="E.g.: your_user_name321"
-            />
-            <Input
-                type="password"
-                name="pass"
-                value={form.pass}
-                onChange={handleInputChange}
-                label="Passowrd"
-            />
-            <Button>
-                Login
-            </Button>
-        </Form>
+        <>
+            {loading === PENDING && <div>login in progress...</div>}
+            {loading !== PENDING &&
+            <Form title="Login - x-stock" onSubmit={handleLogin}>
+                <Input
+                    label="User"
+                    name="user"
+                    value={form.user}
+                    onChange={handleInputChange}
+                    placeholder="E.g.: your_user_name321"
+                />
+                <Input
+                    type="password"
+                    name="pass"
+                    value={form.pass}
+                    onChange={handleInputChange}
+                    label="Password"
+                />
+                <Button>
+                    Login
+                </Button>
+            </Form>
+            }
+        </>
     )
 }
 
--- Section-others/React-router/src/http/http.ts
+++ Section-others/React-router/src/http/http.ts
@@ -1,22 +1,27 @@
 import axios from "axios";
+import {RootState} from "../redux";
+
+let rootState: RootState
+export const injectStore = (_rootState: RootState) => {
+    rootState = _rootState
+}
 
 // create a new Instance of axios, using a preconfigured configs that we can have
+// https://redux.js.org/faq/code-structure#how-can-i-use-the-redux-store-in-non-component-files
 const Http = axios.create({
     baseURL: 'http://localhost:3024',
     /*
     headers: {
         Authorization: 'Bearer 123'
     }
-     */
+    */
 })
 
 Http.interceptors.request.use(config => {
-    console.log(`JWT-0:`)
-    const accessToken = 'MY-DUMMY'
-
-    //const useAppSelector1  = useAppSelector(getJwtToken);
-
-    console.log(`JWT-1:`)
+    let accessToken = ''
+    if (rootState) {
+        accessToken = rootState.authentication.user?.jwtToken || ''
+    }
 
     return {
         ...config,
