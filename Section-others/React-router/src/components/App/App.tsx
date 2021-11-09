9f41f5287d92b0e8d5465dddd0c8cf9dbef589b8
Joao Morgado <joao.morgado@talkdesk.com>
2021-11-09 13:20:41 +0000

    5.2: React route first route

--- Section-others/React-router/src/components/App/App.tsx
+++ Section-others/React-router/src/components/App/App.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import Header from "../Header";
-import Container from "../shared/Container";
-import {TableHeader} from "../shared/Table";
-import {Product} from "../../model/Product";
-import ProductsCRUD from "../Products/ProductsCRUD";
+import HomeView from "../../views/HomeView";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // function TestComponent() {
 //     return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon"/>
 // }
 
-const headers: TableHeader[] = [
-    {key: '_id', value: '#'},
-    {key: 'name', value: 'Product'},
-    {key: 'price', value: 'Price', right: true},
-    {key: 'stock', value: 'Available Stock', right: true}
-]
-
-const newEmptyProduct = (): Product => ({
-    _id: undefined,
-    name: '',
-    price: 1.0,
-    stock: 0,
-    createAt: undefined,
-    updatedAt: undefined
-});
-
 function App() {
 
     return (
         <div>
-            <Header title="x-stocks"/>
-            <Container>
-                <ProductsCRUD/>
-            </Container>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomeView/>}/>
+
+                    <Route
+                        path="*"
+                        element={
+                            <main style={{ padding: "1rem" }}>
+                                <p>There's nothing here!</p>
+                            </main>
+                        }
+                    />
+                </Routes>
+
+            </BrowserRouter>
+
         </div>
     );
 }
