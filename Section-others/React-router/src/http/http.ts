14fd685bbc5d04ddce2b32bc9022027af3581b1d
Joao Costa <costajlmpp@gmail.com>
2021-11-12 18:35:25 +0000

    Using Environment variable to define base url

--- Section-others/React-router/src/http/http.ts
+++ Section-others/React-router/src/http/http.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {RootState} from "../redux";
 
+const { REACT_APP_API_GW_URL } = process.env
+
 let rootState: RootState
 export const injectStore = (_rootState: RootState) => {
     rootState = _rootState
@@ -9,8 +11,9 @@ export const injectStore = (_rootState: RootState) => {
 // create a new Instance of axios, using a preconfigured configs that we can have
 // https://redux.js.org/faq/code-structure#how-can-i-use-the-redux-store-in-non-component-files
 const Http = axios.create({
-    baseURL: 'http://api-gw:3024',
+    baseURL: REACT_APP_API_GW_URL,
     /*
+    baseURL: REACT_APP_API_GW_URL ? REACT_APP_API_GW_URL : 'http://localhost:3024',
     baseURL: 'http://localhost:3024',
     headers: {
         Authorization: 'Bearer 123'
